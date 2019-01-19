619d64f3cde316191c60ead5127a8daff211393c
Heliovic <1005031096@qq.com>
2019-01-19 22:16:45 +0800

    Split data set and create tree

--- chapter03/test_trees.py
+++ chapter03/test_trees.py
@@ -6,9 +6,15 @@ data_set = [
     [1, 0, 'no'],
     [0, 1, 'no'],
     [0, 1, 'no']]
-#
-# labels = ['no surfing', 'flippers']
-#
+
+# 以下标签对应数据集的两个特征
+labels = ['no surfacing', 'flippers']
+
 # print(calc_shannon_ent(data_set))
 
-print(split_data_set(data_set, 0, 1))
+# print(split_data_set(data_set, 0, 1))
+
+# print(choose_best_feature_to_split(data_set))
+
+my_tree = create_tree(data_set, labels)
+print(my_tree)
--- chapter03/trees.py
+++ chapter03/trees.py
@@ -1,4 +1,5 @@
 from math import log
+import operator
 
 
 def calc_shannon_ent(data_set):     # 获取香农熵
@@ -16,6 +17,7 @@ def calc_shannon_ent(data_set):     # 获取香农熵
     return shannon_ent
 
 
+# 按照axis分量为value抽取子集
 def split_data_set(data_set, axis, value):
     ret_data = []
     for feat_vec in data_set:
@@ -26,3 +28,70 @@ def split_data_set(data_set, axis, value):
             # print(reduced_feat_vec)
             ret_data.append(reduced_feat_vec)
     return ret_data
+
+
+'''
+data格式：
+[d1, d2, ..., dn, label (Y/N)]
+'''
+
+
+# 选择可以使熵最低（数据混乱度最低）的划分特征
+def choose_best_feature_to_split(data_set):
+    feature_num = len(data_set[0]) - 1
+    base_ent = calc_shannon_ent(data_set)   # 计算原熵
+    best_info_gain = 0.0
+    best_feature = -1
+    for i in range(feature_num):
+        # 取数据集内每个数据项的第i个特征，feat：feature
+        feat_list = [example[i] for example in data_set]
+        unique_vals = set(feat_list)    # 作为集合，去掉重复
+        new_ent = 0.0   # 新熵
+        # 求解用第 i 个特征划分时的熵
+        for value in unique_vals:
+            # 取出第 i 个特征为 value 的集合作为子集
+            sub_data_set = split_data_set(data_set, i, value)
+            # 计算该子集出现频率
+            prob = len(sub_data_set) / float(len(data_set))
+            # 求和
+            new_ent += prob * calc_shannon_ent(sub_data_set)
+        # 记录最优划分 i
+        info_gain = base_ent - new_ent
+        if info_gain > best_info_gain:
+            best_info_gain = info_gain
+            best_feature = i
+    return best_feature
+
+
+# 返回出现频率最高的分类名称
+def majority_cnt(class_list):
+    class_count = {}
+    for vote in class_list:
+        if vote not in class_count.keys():
+            class_count[vote] = 0
+        class_count[vote] += 1
+    sorted_class_count = sorted(class_count.items(), key=operator.itemgetter(1), reverse=True)
+    return sorted_class_count[0][0]
+
+
+def create_tree(data_set, labels):
+    class_list = [example[-1] for example in data_set]
+    # 当前数据集只有一类标签了，即，数据都是同一类的
+    if class_list.count(class_list[0]) == len(class_list):
+        return class_list[0]
+    # 数据集被分得只剩标签项
+    if len(data_set[0]) == 1:
+        return majority_cnt(data_set)
+
+    best_feat = choose_best_feature_to_split(data_set)
+    best_feat_label = labels[best_feat]
+    my_tree = {best_feat_label: {}}
+    # 从标签中删除被选中作根节点的标签
+    del(labels[best_feat])
+    # 根据最优特征，将数据集分类递归构造决策树
+    feat_values = [example[best_feat] for example in data_set]
+    unique_values = set(feat_values)
+    for value in unique_values:
+        sub_labels = labels[:]
+        my_tree[best_feat_label][value] = create_tree(split_data_set(data_set, best_feat, value), sub_labels)
+    return my_tree
